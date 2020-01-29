61a295294923e61b10c7a8fc9e24d8fdb3ac1b8a
sarah <sarah@triggersandsparks.com>
2020-01-29 18:45:26 +0000

    feat: Add header string across top of page.
    Still needs work. Maybe just better to use an image?

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -1,11 +1,63 @@
-import { SkipLink } from '@octopusthink/nautilus';
+import { PageTitle, SkipLink, useTheme } from '@octopusthink/nautilus';
+import { css } from '@emotion/core';
 
 import React from 'react';
 
 const SiteHeader = () => {
+  const theme = useTheme();
+
   return (
-    <header>
+    <header
+      css={css`
+        position: relative;
+        width: calc(100 % + 32px);
+        height: 400px;
+        overflow: hidden;
+        margin: -16px;
+      `}
+    >
       <SkipLink />
+      <div
+        css={css`
+          background: ${theme.colors.accent.primary};
+          width: 100%;
+          height: 100px;
+          transform: rotate(27deg);
+          width: 120%;
+          height: 299px;
+          transform: rotate(17deg);
+          transform-origin: 0;
+          padding: 186px 50px 0 150px;
+          position: absolute;
+          top: -258px;
+          text-align: center;
+        `}
+      >
+        <PageTitle>
+          <span
+            css={css`
+              color: ${theme.colors.neutral.white};
+              display: block;
+              font-size: 7.2rem;
+              line-height: 0.9;
+              text-indent: -100px;
+            `}
+          >
+            Bringing order
+          </span>
+          <span
+            css={css`
+              color: ${theme.colors.accent.primary};
+              display: block;
+              font-size: 7.2rem;
+              line-height: 0.9;
+              text-indent: 100px;
+            `}
+          >
+            to your meetings.
+          </span>
+        </PageTitle>
+      </div>
     </header>
   );
 };
