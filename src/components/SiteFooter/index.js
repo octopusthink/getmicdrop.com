9b207434ceaccb7173aa895d29bd0a23b356f185
sarah <sarah@triggersandsparks.com>
2020-01-30 15:30:57 +0000

    feat: Add a site footer.

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,7 +1,29 @@
+import { css } from '@emotion/core';
+import { Link, metadata, useTheme } from '@octopusthink/nautilus';
 import React from 'react';
 
 const SiteFooter = () => {
-  return <footer></footer>;
+  const theme = useTheme();
+
+  return (
+    <footer
+      css={css`
+        ${metadata.small(theme)};
+        border-top: 4px solid ${theme.colors.neutral.black};
+        margin: 8rem auto 0;
+        max-width: ${theme.site.maxContentWidth};
+        padding: 1.6rem;
+        text-align: center;
+      `}
+    >
+      Made with ❤️ and 🐙 by{' '}
+      <Link as="a" href="https://octopusthink.com">
+        Octopus Think
+      </Link>
+      {` · `}
+      <Link to="/privacy">Privacy policy</Link>
+    </footer>
+  );
 };
 
 export default SiteFooter;
