b6f7866aa245da0169576a55e79a457466079d07
sarah <sarah@triggersandsparks.com>
2020-03-02 00:50:25 +0000

    chore: Nix unused alt prop.

--- src/components/TextBlock/index.js
+++ src/components/TextBlock/index.js
@@ -4,18 +4,13 @@ import { css } from '@emotion/core';
 import Arrow from 'src/images/arrow.svg';
 
 const TextBlock = (props) => {
-  const { alt, children, end } = props;
+  const { children, end } = props;
 
   return (
     <div
       css={css`
         margin-bottom: 8rem;
         position: relative;
-
-        ${alt &&
-          css`
-            color: white !important;
-          `}
 
         ${end &&
           css`
@@ -29,31 +24,29 @@ const TextBlock = (props) => {
           `}
       `}
     >
-      {!alt && (
-        <img
-          css={css`
-            width: 6.4rem;
-            height: 6.4rem;
-            position: absolute;
+      <img
+        css={css`
+          width: 6.4rem;
+          height: 6.4rem;
+          position: absolute;
 
-            ${end &&
-              css`
-                transform: rotate(-45deg);
-                left: -6.8rem;
-                top: 0;
-              `}
+          ${end &&
+            css`
+              transform: rotate(-45deg);
+              left: -6.8rem;
+              top: 0;
+            `}
 
-            ${!end &&
-              css`
-                transform: rotate(45deg);
-                left: -0.4rem;
-                top: -6.8rem;
-              `}
-          `}
-          src={Arrow}
-          alt=""
-        />
-      )}
+          ${!end &&
+            css`
+              transform: rotate(45deg);
+              left: -0.4rem;
+              top: -6.8rem;
+            `}
+        `}
+        src={Arrow}
+        alt=""
+      />
 
       {children}
     </div>
