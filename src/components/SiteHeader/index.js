f2069d50770a37375974322716c6d57da5d99f3a
sarah <sarah@triggersandsparks.com>
2020-03-12 13:31:39 +0000

    feat: Add a smaller header for non-homepage pages.
    Fix #19.

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -1,25 +1,34 @@
-import { heading, Link, SkipLink, useTheme } from '@octopusthink/nautilus';
+import { Link, SkipLink, useTheme } from '@octopusthink/nautilus';
 import { css } from '@emotion/core';
 import React from 'react';
 
 import Logo from 'src/images/logo.svg';
+import LogoHorizontal from 'src/images/logo-horizontal.svg';
 
-const SiteHeader = () => {
+const SiteHeader = (props) => {
+  const { homepage } = props;
   const theme = useTheme();
 
   return (
     <header
       css={css`
         background: ${theme.colors.neutral.black};
-        background-image: url(/images/header-background.jpg);
-        background-size: cover;
-        background-position: center center;
         width: 100%;
-        min-height: 70vh;
+        min-height: 20vh;
+        margin-bottom: 4.8rem;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
+
+        ${homepage &&
+          css`
+            background-image: url(/images/header-background.jpg);
+            background-position: center center;
+            background-size: cover;
+            min-height: 70vh;
+            margin-bottom: 0;
+          `}
       `}
     >
       <SkipLink />
@@ -28,12 +37,17 @@ const SiteHeader = () => {
         to="/"
         css={css`
           width: 100%;
-          max-width: 80rem;
+          max-width: 48rem;
           padding: 2.4rem;
+
+          ${homepage &&
+            css`
+              max-width: 80rem;
+            `}
         `}
       >
         <img
-          src={Logo}
+          src={homepage ? Logo : LogoHorizontal}
           alt="Mic Drop"
           css={css`
             max-width: 48rem;
@@ -42,32 +56,6 @@ const SiteHeader = () => {
           `}
         />
       </Link>
-      <div
-        css={css`
-          ${heading.small(theme)};
-          color: ${theme.colors.neutral.white};
-          width: 100%;
-          max-width: 520px;
-          padding-left: 1.9em;
-          display: none;
-        `}
-      >
-        Quick, turn your mic off!
-        <span
-          css={css`
-            display: none;
-          `}
-        >
-          Quieter. Calmer. Better.
-        </span>
-        <span
-          css={css`
-            display: none;
-          `}
-        >
-          The menubar app for controlling your microphone.
-        </span>
-      </div>
     </header>
   );
 };
