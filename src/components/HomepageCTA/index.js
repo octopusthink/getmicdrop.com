fd5acd8aae9237c48a6b5233d5bcc78481cdd95a
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-04-15 01:33:08 +0100

    feat: Add download and purchase links

--- src/components/HomepageCTA/index.js
+++ src/components/HomepageCTA/index.js
@@ -1,50 +1,76 @@
 import { Button, Heading, Paragraph, useTheme } from '@octopusthink/nautilus';
 import { css } from '@emotion/core';
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
 const HomepageCTA = () => {
   const theme = useTheme();
 
-  return (
-    <div
-      css={css`
-        margin: 0 auto;
-        max-width: 56rem;
-        text-align: center;
+  const openPaddle = (event) => {
+    event.preventDefault();
 
-        strong {
-          background: linear-gradient(
-            to top,
-            transparent,
-            transparent 0.8rem,
-            ${theme.colors.accent.primary} 0.8rem,
-            ${theme.colors.accent.primary} 2rem,
-            transparent 2rem,
-            transparent
-          );
-          font-weight: 600;
-        }
-      `}
-    >
-      <Heading inverse>
-        Quickly <strong>mute</strong> your microphone with a global <strong>shortcut</strong> or
-        menu bar control
-      </Heading>
+    if (!global.Paddle) {
+      global.console.warn('Paddle global not available.');
+      return;
+    }
+
+    global.Paddle.Setup({ vendor: 104629 });
+    global.Paddle.Checkout.open({
+      message: "50% of sales go to Scottish Women's Aid & Social Bite",
+      product: 576284,
+    });
+  };
+
+  return (
+    <Fragment>
+      <Helmet>
+        <script src="https://cdn.paddle.com/paddle/paddle.js" />
+      </Helmet>
 
       <div
         css={css`
-          margin-bottom: 2.4rem;
+          margin: 0 auto;
+          max-width: 56rem;
+          text-align: center;
+
+          strong {
+            background: linear-gradient(
+              to top,
+              transparent,
+              transparent 0.8rem,
+              ${theme.colors.accent.primary} 0.8rem,
+              ${theme.colors.accent.primary} 2rem,
+              transparent 2rem,
+              transparent
+            );
+            font-weight: 600;
+          }
         `}
       >
-        <Button>Free Download</Button>
-        <Button primary>Buy Now</Button>
-      </div>
+        <Heading inverse>
+          Quickly <strong>mute</strong> your microphone with a global <strong>shortcut</strong> or
+          menu bar control
+        </Heading>
 
-      <Paragraph inverse small>
-        Also available on the Mac App Store. <br />
-        Requires macOS 10.15 Catalina.
-      </Paragraph>
-    </div>
+        <div
+          css={css`
+            margin-bottom: 2.4rem;
+          `}
+        >
+          <Button as="a" href="/downloads/Mic%20Drop%201.0.0.zip" navigation>
+            Free Download
+          </Button>
+          <Button onClick={openPaddle} primary>
+            Buy Now
+          </Button>
+        </div>
+
+        <Paragraph inverse small>
+          {/* Also available on the Mac App Store. <br /> */}
+          Requires macOS 10.15 Catalina.
+        </Paragraph>
+      </div>
+    </Fragment>
   );
 };
 
