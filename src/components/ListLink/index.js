2b752242c3ea0f14054b9bbef60c2cce837387ca
sarah semark <sarah@octopusthink.com>
2020-04-22 14:05:13 +0100

    chore: Remove unused import.

--- src/components/ListLink/index.js
+++ src/components/ListLink/index.js
@@ -1,4 +1,4 @@
-import { Icon, Link, VisuallyHidden, interfaceUI, heading, useTheme } from '@octopusthink/nautilus';
+import { Icon, Link, VisuallyHidden, heading, useTheme } from '@octopusthink/nautilus';
 import React, { useState } from 'react';
 import { css } from '@emotion/core';
 
