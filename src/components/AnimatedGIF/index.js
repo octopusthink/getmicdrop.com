0c180e88c4e8202470848f1bddc2f7b6d859d0f1
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-06-03 01:45:05 +0100

    fix: Status Window GIF in WebKit

--- src/components/AnimatedGIF/index.js
+++ src/components/AnimatedGIF/index.js
@@ -5,16 +5,16 @@ import menubarZoom from 'images/on-off-menubar.gif';
 import statusWindowOn from 'images/status-window-on.gif';
 
 const AnimatedGIF = (props) => {
-  const { alt, src } = props;
+  const { alt, className, src } = props;
   switch (src) {
     case 'images/status-window-on.gif':
-      return <img loading="lazy" alt={alt} src={statusWindowOn} />;
+      return <img className={className} loading="lazy" alt={alt} src={statusWindowOn} />;
     case 'images/on-off-menubar.gif':
-      return <img loading="lazy" alt={alt} src={menubarZoom} />;
+      return <img className={className} loading="lazy" alt={alt} src={menubarZoom} />;
 
     case 'images/menubar-toggling.gif':
     default:
-      return <img loading="lazy" alt={alt} src={menubarToggling} />;
+      return <img className={className} loading="lazy" alt={alt} src={menubarToggling} />;
   }
 };
 
--- src/components/StatusWindow/index.js
+++ src/components/StatusWindow/index.js
@@ -1,43 +1,21 @@
 import { css } from '@emotion/core';
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import AnimatedGIF from 'components/AnimatedGIF';
-import Image from 'components/Image';
 
 const StatusWindow = () => {
-  const [statusWindowOn, setStatusWindowOn] = useState(true);
-
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div
+    <AnimatedGIF
       css={css`
-        display: flex;
-        justify-content: center;
         max-width: 500px;
         width: 100%;
 
         @media screen and (max-width: 500px) {
           max-width: 320px;
         }
-
-        ${!statusWindowOn &&
-          css`
-            margin-left: 14px;
-          `}
       `}
-      // onClick={() => {
-      //   setStatusWindowOn(!statusWindowOn);
-      // }}
-    >
-      {statusWindowOn && (
-        <AnimatedGIF
-          src="images/status-window-on.gif"
-          alt="Status window showing On Air text subtly animating."
-        />
-      )}
-      {/* {!statusWindowOn && (
-        <img alt="Status window showing Off Air text." src="/images/status-window-off.png" />
-      )} */}
-    </div>
+      src="images/status-window-on.gif"
+      alt="Status window showing On Air text subtly animating."
+    />
   );
 };
 
